fbf9a7f898a9eaf66d046447f1d3ecd8648b6fd6
Michael Sloane <oldmansloane@gmail.com>
2017-02-04 13:05:57 -0500

    first commit

---
+++ client/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+      <nav className="nav navbar-default">
+        <div className="navbar-header">
+          <a className="navbar-brand">Shorten My Link</a>
+        </div>
+      </nav>
+  );
+};
+export default Header;
